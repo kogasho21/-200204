3075728a312380c0371dd763e5bfdfc6396b7439
古賀将太 <>
2020-01-29 21:50:53 +0900

    コミットメッセージ

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,7 +14,37 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('bbc', 'PostsController');
+Route::resource('mypage', 'MypageController');
+Route::resource('akiya', 'AkiyaController');
+Route::resource('builder', 'BuilderController');
+Route::resource('build', 'BuildController');
+
+Route::get('/akiya/{id}/show', 'AkiyaController@show');
+Route::get('/akiya', 'AkiyaController@index');
+
+Route::get('/build/{id}/show', 'BuildController@show');
+Route::get('/build', 'BuildController@index');
+
+Route::get('/build/create', 'BuildController@create');
+Route::post('/build/create', 'BuildController@store');
+
+Route::get('/builder/{id}/show', 'BuilderController@show');
+Route::get('/builder', 'BuilderController@index');
+
+Route::get('/builder/create', 'BuilderController@create');
+Route::post('/builder/create', 'BuildController@store');
+
+
+Route::resource('comments', 'CommentsController');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/users', function () {
+    return App\User::all();
+});
