9f9d35fc59b246b81f331c87f193771e690765e4
古賀将太 <>
2020-01-29 22:57:05 +0900

    コミットメッセージ

--- src/resources/views/akiya/index.blade.php
+++ src/resources/views/akiya/index.blade.php
@@ -2,20 +2,20 @@
 @section('content')
 
 <div class="col-xs-8 col-xs-offset-2">
-	<div class="mb-4">
-	@foreach ($akiyas as $akiya)
-		<p>{{ link_to("/akiya/{$akiya->id}/show", '続きを読む', array('class' => 'btn btn-primary')) }}</p>
-		{{ $akiya->akiya_id }}<br>
-		{{ $akiya->location }}<br>
-		{{ $akiya->landarea }}<br>
-		{{ $akiya->drivewayarea }}<br>
-		{{ $akiya->buildingarea }}<br>
-		{{ $akiya->buildingstructure }}<br>
-		@foreach ($akiya->image_paths as $image_path)
-			<img width="100" src="{{ $image_path }}">
-		@endforeach
-	@endforeach
-	</div>
+    <div class="mb-4">
+        @foreach ($akiyas as $akiya)
+        <p>{{ link_to("/akiya/{$akiya->id}/show", '続きを読む', array('class' => 'btn btn-primary')) }}</p>
+        {{ $akiya->akiya_id }}<br>
+        {{ $akiya->location }}<br>
+        {{ $akiya->landarea }}<br>
+        {{ $akiya->drivewayarea }}<br>
+        {{ $akiya->buildingarea }}<br>
+        {{ $akiya->buildingstructure }}<br>
+        @foreach ($akiya->image_paths as $image_path)
+        <img width="100" src="{{ $image_path }}">
+        @endforeach
+        @endforeach
+    </div>
 </div>
 
 @stop
