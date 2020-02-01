70c5d74e4621b1fa725e9ce362502de36172996f
古賀将太 <>
2020-02-02 01:21:49 +0900

    コミットメッセージ

--- src/resources/views/akiya/index.blade.php
+++ src/resources/views/akiya/index.blade.php
@@ -1,20 +1,64 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="col-xs-8 col-xs-offset-2">
-    <div class="mb-4">
-        @foreach ($akiyas as $akiya)
-        {{ $akiya->akiya_id }}<br>
-        {{ $akiya->location }}<br>
-        {{ $akiya->landarea }}<br>
-        {{ $akiya->drivewayarea }}<br>
-        {{ $akiya->buildingarea }}<br>
-        {{ $akiya->buildingstructure }}<br>
-        @foreach ($akiya->image_paths as $image_path)
-        <img width="100" src="{{ $image_path }}">
-        @endforeach
-        @endforeach
-    </div>
+<div class="l-content">
+    <main class="l-main" role="main">
+        <section>
+            <h2 class="c-title">空き家を探す</h2>
+            <form class="" method="post">
+                <div class="place_select">
+                    <select>
+                        <option value="八女市">
+                            八女市</option>
+                        <option value="八女市">
+                            八女市</option>
+                    </select>
+                </div>
+                <div class="mapArea">
+                    <img src="../assets/img/map.png" alt="">
+                </div>
+
+                <p class="c-title"><span>八女市</span>の空き家(○○件)</p>
+                <div class="c-house__list">
+                    <div class="house__item">
+                        <div class="item__img">
+                            <img src="../assets/img/dummy_01.jpg" alt="">
+                        </div>
+                        <div class="item__txt">
+                            <dl>
+                                @foreach ($akiyas as $akiya)
+                                {{ $akiya->akiya_id }}
+                                <dt>所在地：</dt>
+                                <dd>{{ $akiya->location }}</dd>
+                                <dt>⼟地⾯積：</dt>
+                                <dd>{{ $akiya->landarea }}</dd>
+                                <dt>私道⾯積：</dt>
+                                <dd>{{ $akiya->drivewayarea }}</dd>
+                                <dt>建物⾯積：</dt>
+                                <dd>{{ $akiya->buildingarea }}</dd>
+                                <dt>建物構造：</dt>
+                                <dd>{{ $akiya->buildingstructure }}</dd>
+                                @foreach ($akiya->image_paths as $image_path)
+                                <img width="100" src="{{ $image_path }}">
+                                @endforeach
+                                @endforeach
+                            </dl>
+                            <span class="icon-fav"></span>
+                        </div>
+
+                    </div>
+                </div>
 </div>
+</form>
+
+</section>
+
+</main>
+
+</div><!-- /.l-content end -->
+
+</body>
+
+</html>
 
 @stop
