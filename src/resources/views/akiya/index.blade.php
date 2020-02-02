862a7695209c6a792ec94637c224681dd1e454ec
古賀将太 <>
2020-02-02 17:40:16 +0900

    コミットメッセージ

--- src/resources/views/akiya/index.blade.php
+++ src/resources/views/akiya/index.blade.php
@@ -73,7 +73,8 @@
 
                         @foreach ($akiyas as $akiya)
                         <div class="house__item">
-                            <div class="house__item">
+
+                            <div class="item__txt">
                                 <!-- {{ $akiya->akiya_id }} -->
                                 <dt>所在地：</dt>
                                 <dd>{{ $akiya->location }}</dd>
