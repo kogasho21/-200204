3802bd6851c73f0158f82e786bb8980ed05deae2
古賀将太 <>
2020-02-02 20:02:58 +0900

    コミットメッセージ

--- src/database/migrations/2020_01_02_171031_create_builders_table.php
+++ src/database/migrations/2020_01_02_171031_create_builders_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateBuildersTable extends Migration
 {
@@ -14,16 +14,19 @@ class CreateBuildersTable extends Migration
     public function up()
     {
         Schema::create('builders', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name'); //名前
-            $table->string('office'); //事業所名
-            $table->string('address'); //住所
-            $table->string('mail_address'); //メールアドレス
-            $table->string('tell_no'); //電話番号
-            $table->string('unisex'); //性別
-            $table->date('birthday'); //誕生日
-            $table->string('homepage'); //ホームページ
-            $table->text('career'); //経歴
+            $table->bigIncrements('id')->comment('建築者ID');
+            $table->string('name')->comment('名前');
+            $table->tinyInteger('sex')->comment('性別');
+            $table->date('birth_dt')->comment('誕生日');
+            $table->string('occupation')->nullable()->comment('職業');
+            $table->string('tel')->nullable()->comment('電話番号');
+            $table->text('comment')->nullable()->comment('自己紹介');
+            $table->string('email')->unique()->comment('メールアドレス');
+            $table->string('url')->nullable()->comment('WebサイトURL');
+            $table->boolean('status')->default(1)->comment('状態');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
