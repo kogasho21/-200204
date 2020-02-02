281083a3669e847af8079314f337ac9853ee5171
古賀将太 <>
2020-02-02 18:06:50 +0900

    コミットメッセージ

--- src/resources/views/akiya/index.blade.php
+++ src/resources/views/akiya/index.blade.php
@@ -60,8 +60,126 @@
                         <select>
                             <option value="八女市">
                                 八女市</option>
+                            <option value="北九州市">
+                                北九州市</option>
+                            <option value="福岡市">
+                                福岡市</option>
+                            <option value="大牟田市">
+                                大牟田市</option>
+                            <option value="久留米市">
+                                久留米市</option>
+                            <option value="直方市">
+                                直方市</option>
+                            <option value="飯塚市">
+                                飯塚市</option>
+                            <option value="田川市">
+                                田川市</option>
+                            <option value="柳川市">
+                                柳川市</option>
                             <option value="八女市">
                                 八女市</option>
+                            <option value="筑後市">
+                                筑後市</option>
+                            <option value="大川市">
+                                大川市</option>
+                            <option value="行橋市">
+                                行橋市</option>
+                            <option value="豊前市">
+                                豊前市</option>
+                            <option value="中間市">
+                                中間市</option>
+                            <option value="小郡市">
+                                小郡市</option>
+                            <option value="筑紫野市">
+                                筑紫野市</option>
+                            <option value="春日市">
+                                春日市</option>
+                            <option value="大野城市">
+                                大野城市</option>
+                            <option value="宗像市">
+                                宗像市</option>
+                            <option value="太宰府市">
+                                太宰府市</option>
+                            <option value="古賀市">
+                                古賀市</option>
+                            <option value="福津市">
+                                福津市</option>
+                            <option value="うきは市">
+                                うきは市</option>
+                            <option value="宮若市">
+                                宮若市</option>
+                            <option value="嘉麻市">
+                                嘉麻市</option>
+                            <option value="朝倉市">
+                                朝倉市</option>
+                            <option value="みやま市">
+                                みやま市</option>
+                            <option value="糸島市">
+                                糸島市</option>
+                            <option value="那珂川市">
+                                那珂川市</option>
+                            <option value="宇美町">
+                                宇美町</option>
+                            <option value="篠栗町">
+                                篠栗町</option>
+                            <option value="志免町">
+                                志免町</option>
+                            <option value="須恵町">
+                                須恵町</option>
+                            <option value="新宮町">
+                                新宮町</option>
+                            <option value="久山町">
+                                久山町</option>
+                            <option value="粕屋町">
+                                粕屋町</option>
+                            <option value="芦屋町">
+                                芦屋町</option>
+                            <option value="水巻町">
+                                水巻町</option>
+                            <option value="岡垣町">
+                                岡垣町</option>
+                            <option value="遠賀町">
+                                遠賀町</option>
+                            <option value="小竹町">
+                                小竹町</option>
+                            <option value="鞍手町">
+                                鞍手町</option>
+                            <option value="桂川町">
+                                桂川町</option>
+                            <option value="筑前町">
+                                筑前町</option>
+                            <option value="東峰村">
+                                東峰村</option>
+                            <option value="大刀洗町">
+                                大刀洗町</option>
+                            <option value="大木町">
+                                大木町</option>
+                            <option value="広川町">
+                                広川町</option>
+                            <option value="香春町">
+                                香春町</option>
+                            <option value="添田町">
+                                添田町</option>
+                            <option value="糸田町">
+                                糸田町</option>
+                            <option value="川崎町">
+                                川崎町</option>
+                            <option value="大任町">
+                                大任町</option>
+                            <option value="赤村">
+                                赤村</option>
+                            <option value="福智町">
+                                福智町</option>
+                            <option value="苅田町">
+                                苅田町</option>
+                            <option value="みやこ町">
+                                みやこ町</option>
+                            <option value="吉富町">
+                                吉富町</option>
+                            <option value="上毛町">
+                                上毛町</option>
+                            <option value=" 築上町">
+                                築上町</option>
                         </select>
                     </div>
                     <div class="mapArea">
