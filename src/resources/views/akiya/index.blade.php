85177c662434775a3380a61b1e9951e048a6aca9
古賀将太 <>
2020-02-02 18:35:52 +0900

    コミットメッセージ

--- src/resources/views/akiya/index.blade.php
+++ src/resources/views/akiya/index.blade.php
@@ -181,7 +181,7 @@
                         </select>
                     </div>
                     <div class="mapArea">
-                        <img src="{{ asset('/img/map.svg') }}" alt="">
+                        <img src="{{ asset('/img/map.svg') }}" alt="" width="70%">
                     </div>
 
                     <p class="c-title">空き家(24件)</p>
